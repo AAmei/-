66149b28f56fd56e422df0d079d9818eef262611
Amei <mogalist@163.com>
2017-05-04 20:47:53 +0800

    Update 读取数量不定的输入数据.cpp

--- 读取数量不定的输入数据.cpp
+++ 读取数量不定的输入数据.cpp
@@ -9,19 +9,13 @@ int main()
 	//读取数据直到遇到文件尾，计算所有读入值的和
 	while(cin>>c)
 		sum += c;
-	
-
 
 	cout<<"sum is : "<<sum<<endl;
 
-
 	char cc = '0';
-
-
 	while(cin>>cc)
 		cout<<cc;
 
 	cout<<"over";
-	while(1);
 	return 0;
 }
