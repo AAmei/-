1bb9191c0f841c952c4cb45fa6cad31a99439f4f
Amei <mogalist@163.com>
2017-05-04 19:54:41 +0800

    Create 算法提高c++_ch02_01.cpp

---
+++ 算法提高c++_ch02_01.cpp
@@ -0,0 +1,10 @@
+#include "iostream"  
+using namespace std;
+int main()  
+{  
+    char yuan[12]={'A', 'E', 'I', 'O', 'U', 'a', 'e', 'i', 'o', 'u'};   
+    for(int i=0;i<10;i++)  
+        cout <<(int)yuan[i] << endl;  
+    cout << endl; 
+    return 0;  
+}
